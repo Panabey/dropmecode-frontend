6ed8f49f755315b2c9fa7d8271a5d5ef624f0051
sanua356 <sanek.pankratov356@gmail.com>
2023-08-10 10:33:38 +0300

    add clear searchbar icon

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,16 @@
-import { IoSearch } from 'react-icons/io5'
-import s from './SearchBar.module.css'
+import { useState } from 'react';
+import { IoClose, IoSearch } from 'react-icons/io5';
+import s from './SearchBar.module.css';
 
 export const SearchBar = () => {
+
+	const [searchValue, setSearchValue] = useState<string>('');
+
 	return (
 		<div className={s.search}>
+			{searchValue.length > 0 && <IoClose className={s.icon_clear} fill="#000" size={22} onClick={() => setSearchValue('')} />}
 			<IoSearch fill="#000" size={20} />
-			<input type="text" className={s.search__input} placeholder='Поиск по разделам...' />
+			<input type="text" className={s.search__input} placeholder='Поиск по разделам...' value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
 		</div>
 	)
 }
