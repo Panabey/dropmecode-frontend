33cd78671e15567972c4cbe616deadc2c394579b
sanua356 <sanek.pankratov356@gmail.com>
2023-10-15 14:29:04 +0300

    add search by tags

---
+++ next_app/src/pages/confidentiality.tsx
@@ -0,0 +1,9 @@
+import { ConfidentialityPageBuilder } from '@/screens/Confidentiality/ConfidentialityPageBuilder'
+
+const ConfidentialityPage = () => {
+	return (
+		<ConfidentialityPageBuilder />
+	)
+}
+
+export default ConfidentialityPage
