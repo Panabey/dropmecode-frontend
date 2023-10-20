508488f18dc5618c06bf58982a1ce5d745415179
sanua356 <sanek.pankratov356@gmail.com>
2023-10-20 09:21:56 +0300

    fix footer size

--- next_app/src/components/Footer/Footer.tsx
+++ next_app/src/components/Footer/Footer.tsx
@@ -48,24 +48,28 @@ export const Footer = () => {
 					<Image className={classNames('hidden', 'goose2')} width={512} height={512} alt='Важный гусь' src={'/assets/Goose/goose2.png'} style={{ pointerEvents: 'none' }} />
 				</div>
 				<div className={s.container}>
-					<div className={classNames(s.row)}>
-						<aside>Мы в соцсетях:</aside>
-						<div className={s.row}>
-							<Link href="https://github.com/Panabey">
-								<FaGithub fill="#000" size={20} className={s.icon} />
-							</Link>
-							<Link href="https://vk.com/dropmecode" target="_blank">
-								<Image width={20} height={20} alt='SVG VK' src='/assets/Header/vk1.svg' className={s.icon} />
-							</Link>
+					<div></div>
+					<div className={s.area}>
+						<div className={classNames(s.row)}>
+							<aside>Мы в соцсетях:</aside>
+							<div className={s.row}>
+								<Link href="https://github.com/Panabey">
+									<FaGithub fill="#000" size={20} className={s.icon} />
+								</Link>
+								<Link href="https://vk.com/dropmecode" target="_blank">
+									<Image width={20} height={20} alt='SVG VK' src='/assets/Header/vk1.svg' className={s.icon} />
+								</Link>
+							</div>
+						</div>
+						<div className={classNames(s.row, s.row__copyright)}>
+							<aside>&copy; DropMeCode 2023. Все права защищены.</aside>
+						</div>
+						<div className={classNames(s.row, s.row__buttons)}>
+							<Link href="/confidentiality"><button className={s.button}>Политика конфиденциальности</button></Link>
+							<button className={s.button}>Условия пользования</button>
 						</div>
 					</div>
-					<div className={classNames(s.row, s.row__copyright)}>
-						<aside>&copy; DropMeCode 2023. Все права защищены.</aside>
-					</div>
-					<div className={s.row}>
-						<Link href="/confidentiality"><button className={s.button}>Политика конфиденциальности</button></Link>
-						<button className={s.button}>Условия пользования</button>
-					</div>
+					<div></div>
 				</div>
 			</footer>
 		</>
