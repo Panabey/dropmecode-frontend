db7adb3323826c0179dfbb77af90924c7aefc333
sanua356 <sanek.pankratov356@gmail.com>
2023-10-20 11:12:21 +0300

    add favicon

--- next_app/src/pages/_app.tsx
+++ next_app/src/pages/_app.tsx
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Head>
       <title>DROPMECODE</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <Provider store={store}>
       <NextNProgress color="#1DC989" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
--- next_app/src/pages/_document.tsx
+++ next_app/src/pages/_document.tsx
@@ -7,7 +7,10 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
         <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/Global/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/Global/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/Global/favicon-16x16.png" />
+        <link rel="manifest" href="/assets/Global/site.webmanifest"></link>
       </Head>
       <body>
         <Main />
