f14a133d14c7be1796cfea511a5101216be5c744
sanua356 <sanek.pankratov356@gmail.com>
2023-10-28 19:43:03 +0300

    change meta tag in index page

--- next_app/src/pages/index.tsx
+++ next_app/src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>DROPMECODE | Главная</title>
-        <meta name="description" content="DROPMECODE - позвавательный портал для программистов и интересующихся IT миром. Множество бесплатных и полезных материалов для обучения и развлечений."></meta>
+        <meta name="description" content="DROPMECODE - это справочный ресурс, где можно проверить и повысить свои знания в области программирования"></meta>
         <meta name="keywords" content="DROPMECODE, dropmecode, IT, программирование, портал для программистов, разработка, веб-разработка, базы данных, алгоритмы, структуры данных, компьютерные сети, 
         мобильные приложения, тестирование ПО, защита информации, python, javascript" />
       </Head>
